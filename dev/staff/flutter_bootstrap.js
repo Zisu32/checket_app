8b8a12382d9872d8775be28c47b7d4fc354b2517
Zisu32 <33527857+Zisu32@users.noreply.github.com>
2026-08-18 10:44:07 +0000

    Deploying to gh-pages from @ Zisu32/checket_app@425b30ea9d5d6c464441dc7e62698926eb46b398 🚀

--- dev/staff/flutter_bootstrap.js
+++ dev/staff/flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5f77625673248ee5846fbcaf5d3e1a3878386f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "950136111" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2052405860" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
