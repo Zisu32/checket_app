992794bab0fbdc0542460bddbcdd8e43e47a2d1d
Zisu32 <pascal.bonicho@gmail.com>
2026-09-06 12:11:23 +0200

    new colors

--- lib/shared/theme/app_theme.dart
+++ lib/shared/theme/app_theme.dart
@@ -3,13 +3,13 @@ import 'package:flutter/material.dart';
 class AppTheme {
   // Main colors
   static const Color header = Color(0xFF131314);
-  static const Color background = Color(0xFF1E1F20);
-  static const Color surface = Color(0xFF282A2C);
+  static const Color background = Color(0xFF2D2D30);
+  static const Color surface = Color(0xFF515657);
   
   // Status colors
-  static const Color unpaid = Color(0xFFE57373);
-  static const Color temporary = Color(0xFFFFB74D);
-  static const Color secret = Color(0xFFFFF176);
+  static const Color unpaid = Color(0xFFA15151);
+  static const Color temporary = Color(0xFFCF9941);
+  static const Color secret = Color(0xFFB19C34);
   static const Color active = Color(0xFF00B58B);
   static const Color forgotten = Color(0xFF64B5F6);
   static const Color free = Color(0xFF8E918F);
