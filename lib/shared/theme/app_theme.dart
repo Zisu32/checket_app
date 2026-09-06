672a5503f3ccad53c3785c19fcd9ad6ca317635e
Zisu32 <pascal.bonicho@gmail.com>
2026-09-06 12:44:15 +0200

    new colors 5

--- lib/shared/theme/app_theme.dart
+++ lib/shared/theme/app_theme.dart
@@ -3,8 +3,8 @@ import 'package:flutter/material.dart';
 class AppTheme {
   // Main colors
   static const Color header = Color(0xFF131314);
-  static const Color background = Color(0xFF292B2C);
-  static const Color surface = Color(0xFF3E4045);
+  static const Color background = Color(0xFF292A2C);
+  static const Color surface = Color(0xFF3E3F45);
   
   // Status colors
   static const Color unpaid = Color(0xFFA15151);
